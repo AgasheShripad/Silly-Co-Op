a44dd3ddf0b90b91434e8914a5188ca4ba75d5de
ShripadAgashe <agashe.s@northeastern.edu>
2022-10-25 17:16:01 -0400

    two player characters prefabs added

--- Silly-Co-Op Project/Assets/Scripts/Active Ragdoll/Modules/CameraModule.cs
+++ Silly-Co-Op Project/Assets/Scripts/Active Ragdoll/Modules/CameraModule.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace ActiveRagdoll {
-    // Author: Sergio Abreu García | https://sergioabreu.me
+    
 
     public class CameraModule : Module {
         [Header("--- GENERAL ---")]
